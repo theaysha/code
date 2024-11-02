ab8bf8e6153cf3196efdeb56c077e7a22c4ad902
Mohd Shahid <mohd47149@gmail.com>
2024-11-02 14:29:09 +0530

    add hover

--- ShreeHuman/index.php
+++ ShreeHuman/index.php
@@ -152,7 +152,7 @@
           </div>
         </div>
         <div class="container">
-          <!-- <div class="row">
+          <div class="row">
             <div class="col-sm-12">
               <ul class="filter font-alt" id="filters">
                 <li><a class="current wow fadeInUp" href="#" data-filter="*">All</a></li>
@@ -162,9 +162,10 @@
                 <li><a class="wow fadeInUp" href="#" data-filter=".webdesign" data-wow-delay="0.6s">Mutual Funds</a></li>
               </ul>
             </div>
-          </div> -->
+          </div>
           <ul class="works-grid works-grid-gut works-grid-3 works-hover-d" id="works-grid">
             <li class="work-item illustration webdesign"><a href="portfolio_single_featured_image1.html">
+            <div class="work-image"><img src="assets/images/fund.jpg" alt="Portfolio Item" /></div>
                 <div class="work-caption font-alt">
                   <h3 class="work-title">Start a Mutual Fund SIP</h3>
                   <div class="work-descr">Choose from our wide array of mutual fund schemes catering to various combinations of goals and risk appetites. Start building your portfolio today.</div>
@@ -178,6 +179,7 @@
                 </div>
               </a></li>
             <li class="work-item illustration photography"><a href="portfolio_single_featured_slider1.html">
+            <div class="work-image"><img src="assets/images/goals.jpg" alt="Portfolio Item" /></div>
                 <div class="work-caption font-alt">
                   <h3 class="work-title">Plan Your Goals</h3>
                   <div class="work-descr">We spend time to understand your needs and aspirations and prepare a financial investment plan based on your risk profile and goals to achieve them.</div>
@@ -191,6 +193,7 @@
                 </div>
               </a></li>
             <li class="work-item illustration webdesign"><a href="portfolio_single_featured_video1.html">
+            <div class="work-image"><img src="assets/images/savetax.jpg" alt="Portfolio Item" /></div>
                 <div class="work-caption font-alt">
                   <h3 class="work-title">Invest In ELSS & Save Tax</h3>
                   <div class="work-descr">ELSS = Save Tax + Wealth Creation. ELSS is a tax saving scheme that allows investors to seek some capital appreciation with tax benefits.</div>
